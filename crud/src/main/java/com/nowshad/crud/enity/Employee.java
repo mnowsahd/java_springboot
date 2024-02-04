d167d5a9ef764fa2e600aad4ba69f8b40e3bac4c
Nowshad <mnowshad2010@gmail.com>
2024-02-04 22:49:50 +0530

    application.properties updated

---
+++ crud/src/main/java/com/nowshad/crud/enity/Employee.java
@@ -0,0 +1,16 @@
+package com.nowshad.crud.enity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private Long id;
+
+}
